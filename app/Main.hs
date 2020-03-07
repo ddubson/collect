d3f2d6c0dc9157f816c7c6061feb210b1c39d0d8
Dmitriy Dubson <d.dubson@gmail.com>
2020-03-07 15:01:09 -0500

    Add matching for a Gradle groovy project

--- app/Main.hs
+++ app/Main.hs
@@ -1,11 +1,18 @@
 module Main where
 
 import Lib
-import System.Directory (getCurrentDirectory)
+import Data.List (find)
+import System.Directory (listDirectory, getCurrentDirectory)
+
+gradleGroovyMatch = "build.gradle"
 
 main :: IO ()
 main = do
     -- Read current directory
     -- Identify if build.gradle exists in current directory
     directory <- getCurrentDirectory
-    putStrLn $ directory
+    directoryContents <- (listDirectory directory)
+    let maybeGradleGroovy = find (== gradleGroovyMatch) directoryContents :: Maybe [Char]
+    case maybeGradleGroovy of
+        Nothing -> print "Unable to determine project type."
+        Just x -> print "Gradle (Groovy) project"
